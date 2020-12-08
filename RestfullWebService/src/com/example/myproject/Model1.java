c020bbf23ea946959d78d195e34ad402d7e162d9
Kamlesh.mali <kamlesh.mali@cellulant.com>
2020-12-08 18:53:17 +0530

    Added 2 files

---
+++ RestfullWebService/src/com/example/myproject/Model1.java
@@ -0,0 +1,7 @@
+package com.example.myproject;
+
+public class Model1 {
+
+	public String name ;//By FZ
+	
+}
---
+++ RestfullWebService/src/com/example/myproject/Model2.java
@@ -0,0 +1,6 @@
+package com.example.myproject;
+
+public class Model2 {
+
+pivate String name; //by M
+}
